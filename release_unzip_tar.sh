dc977d1d700f26d03e45247c77acdce96dab6eec
Shamis Shukoor <shamis657@gmail.com>
2019-04-29 17:45:38 +0200

    Release 0.0.2

--- release_unzip_tar.sh
+++ release_unzip_tar.sh
@@ -4,10 +4,10 @@ rm -rf _build deploy/node*
 
 echo "release extract node1"
 rebar3 as node1 tar
-mkdir -p ./deploy/node1-ipro-0.0.1
-tar xvzf _build/node1/rel/ipro/ipro-0.0.1.tar.gz -C ./deploy/node1-ipro-0.0.1
+mkdir -p ./deploy/node1-ipro-0.0.2
+tar xvzf _build/node1/rel/ipro/ipro-0.0.2.tar.gz -C ./deploy/node1-ipro-0.0.2
 
 echo "release extract node2"
 rebar3 as node2 tar
-mkdir -p ./deploy/node2-ipro-0.0.1
-tar xvzf _build/node2/rel/ipro/ipro-0.0.1.tar.gz -C ./deploy/node2-ipro-0.0.1
+mkdir -p ./deploy/node2-ipro-0.0.2
+tar xvzf _build/node2/rel/ipro/ipro-0.0.2.tar.gz -C ./deploy/node2-ipro-0.0.2
